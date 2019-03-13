6791695b61565d6a76ccb134efd23427ccfc7a6b
DIego Villafuerte Soraiz <diegovillafuertesoraiz@gmail.com>
2019-03-12 18:17:05 -0600

    Ya jala el envío de topos

--- topos/src/server/ComputeServer.java
+++ topos/src/server/ComputeServer.java
@@ -23,20 +23,25 @@ import java.io.DataOutputStream;
 import java.io.EOFException;
 import java.io.IOException;
 import java.net.DatagramPacket;
-import java.net.InetAddress;
 import java.net.MulticastSocket;
 import java.net.ServerSocket;
 import java.net.Socket;
 import java.net.SocketException;
 import java.util.ArrayList;
+import java.net.InetAddress;
+import java.net.UnknownHostException;
+import java.util.Random;
+import java.util.concurrent.TimeoutException;
 
 public class ComputeServer implements Compute {
+    
+    
      
-    ArrayList<String> direccionesIP = new ArrayList();
+    static ArrayList<InetAddress> direccionesIP = new ArrayList();
     static int[] score= new int[10];
     static int jugadoresActivos;
-    String ipServer="";
-    int puertoServer=0;
+    static InetAddress ipServer;
+    static int puertoServer=6789;
     static int n=4;
     public ComputeServer() throws RemoteException
     {
@@ -58,7 +63,7 @@ public class ComputeServer implements Compute {
     private static void inicializaRMI(){
         //Aquí se inicia el RMI
         
-        System.setProperty("java.security.policy","/Users/jeroaranda/Documents/ITAM/DECIMO/SISTEMAS DISTR./proyecto01/whack-a-mole/topos/src/server/server.policy");
+        System.setProperty("java.security.policy","/Users/diego/whack-a-mole/topos/src/server/server.policy");
 
         if (System.getSecurityManager() == null) {
 
@@ -86,21 +91,33 @@ public class ComputeServer implements Compute {
             e.printStackTrace();
         }
     }
-    public static void enviarTopo(){
+    
+    private static int getRand(int min, int max) {
+
+            if (min >= max) {
+                    throw new IllegalArgumentException("max must be greater than min");
+            }
+
+            Random r = new Random();
+            return r.nextInt((max - min) + 1) + min;
+    }
+    
+    public static Boolean enviarTopo(){
         //Se envía por multicast el topo;
             MulticastSocket s =null;
+            Boolean respo = true;
             try {
                    InetAddress group = InetAddress.getByName("228.5.6.7"); // destination multicast group 
-                   s = new MulticastSocket(6789);
+                   s = new MulticastSocket(puertoServer);
                    s.joinGroup(group); 
-                   //s.setTimeToLive(10);
-                   System.out.println("Messages' TTL (Time-To-Live): "+ s.getTimeToLive());
-                   String myMessage="Hello";
+                   String resp = String.valueOf(getRand(1,12));
+                   if(alguienHaGanao())
+                       resp = "13";
+                   String myMessage=resp;
                    byte [] m = myMessage.getBytes();
                    DatagramPacket messageOut = 
                            new DatagramPacket(m, m.length, group, 6789);
                    s.send(messageOut);
-
                    s.leaveGroup(group);		
                }
             catch (SocketException e){
@@ -112,25 +129,36 @@ public class ComputeServer implements Compute {
             finally {
                if(s != null) s.close();
            }
+           return respo;
     }
-    public static int esperaGanador(){
+    public static int esperaGanador() throws IOException{
         //Se espera la respuesta de la computadora ganadora y se incrementa el score del ganador, posteriormente se reinicia el juego.
         int ganador=0;
+        int serverPort = 7896;
+        ServerSocket listenSocket = new ServerSocket(serverPort);
         try{
-		int serverPort = 7896; 
-		ServerSocket listenSocket = new ServerSocket(serverPort);
-		while(true) {
-			System.out.println("Waiting for messages..."); 
-                        Socket clientSocket = listenSocket.accept();  // Listens for a connection to be made to this socket and accepts it. The method blocks until a connection is made. 
-			Connection c = new Connection(clientSocket);
-                        c.start();
-                        ganador=c.in.readInt();
-                }
-	} catch(IOException e) {System.out.println("Listen :"+ e.getMessage());}
+            listenSocket.setSoTimeout(3000);
+            while(true) {
+                System.out.println("Waiting for messages..."); 
+                Socket clientSocket = listenSocket.accept();  // Listens for a connection to be made to this socket and accepts it. The method blocks until a connection is made. 
+                Connection c = new Connection(clientSocket);
+                c.start();
+                ganador=c.in.readInt();
+                listenSocket.close();
+                return ganador;  
+            }
+	}
+        catch(IOException e) {
+            if(e.getMessage().equals("Accept timed out")){
+                System.out.println("Hubo timeout de espera ganador");
+                listenSocket.close();
+            }
+            //System.out.println("Listen :"+ e.getMessage());
+        }
         return ganador;
     }
     @Override
-    public void crearSesion(String ipCliente) throws RemoteException {
+    public void crearSesion(InetAddress ipCliente) throws RemoteException {
            if(!direccionesIP.contains(ipCliente)){
                direccionesIP.add(ipCliente);
                jugadoresActivos++;
@@ -138,21 +166,28 @@ public class ComputeServer implements Compute {
     }
 
     @Override
-    public String obtenerIP(String ipCliente) throws RemoteException {
+    public InetAddress obtenerIP(InetAddress ipCliente) throws RemoteException {
         if(direccionesIP.contains(ipCliente))
             return ipServer;
         return null;
     }
+    
+    public String prueba(){
+        return "El rmi funciona";
+    }
 
     @Override
-    public int obtenerPuerto(String ipCliente) throws RemoteException {
+    public int obtenerPuerto(InetAddress ipCliente) throws RemoteException {
         if(direccionesIP.contains(ipCliente))
             return puertoServer;
         return -1;
     }
-    public static void main(String[] args) {
-
+    public static void main(String[] args) throws UnknownHostException, IOException {
+        System.setProperty("java.net.preferIPv4Stack", "true");
+        ipServer = InetAddress.getByName("228.5.6.7");
         inicializaRMI();
+        System.out.println(direccionesIP.toString());
+        
         //Aquí se inicia el envío de topos, es decir el comienzo del juego.
         while(!alguienHaGanao()){
             enviarTopo();
